ebbfe638c6cbad1af8b3823341034b196ea160ec
Iosif Nicolae <iosif@bringes.io>
2025-11-30 20:43:03 +0200

    feat(schedules): add schedules UI components
    Includes:
    - ScheduleList: main list view with search and CRUD actions
    - ScheduleListItem: individual schedule card with actions menu
    - ScheduleModal: create/edit schedule dialog
    - AddToScheduleModal: modal for adding items to schedules
    - All components support dark mode and are fully responsive

---
+++ app/apps/client/src/features/schedules/components/index.ts
@@ -0,0 +1,4 @@
+export { AddToScheduleModal } from './add-to-schedule-modal'
+export { ScheduleList } from './schedule-list'
+export { ScheduleListItem } from './schedule-list-item'
+export { ScheduleModal } from './schedule-modal'
